684255243a30a1ef5b016702b546761948f9d15a
Aaron Contreras <aaronlcaq@gmail.com>
2020-03-05 23:23:17 -0500

    Use ArrayList instead of HashMap

--- src/main/java/House.java
+++ src/main/java/House.java
@@ -1,46 +1,47 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 
 
 public class House {
 
-    HashMap<Integer,String> map = new HashMap<Integer,String>();
+    ArrayList<String> list = new ArrayList<String>();
 
     public House() {
-        map.put(0, "malt that lay in the house that Jack built.");
-        map.put(1, "This is the house that Jack built.");
-        map.put(2, "This is the malt that lay in the house that Jack built.");
-        map.put(3, "rat/ate");
-        map.put(4, "cat/killed");
-        map.put(5, "dog/worried");
-        map.put(6, "cow with the crumpled horn/tossed");
-        map.put(7, "maiden all forlorn/milked");
-        map.put(8, "man all tattered and torn/kissed");
-        map.put(9, "priest all shaven and shorn/married");
-        map.put(10, "rooster that crowed in the morn/woke");
-        map.put(11, "farmer sowing his corn/kept");
-        map.put(12, "horse and the hound and the horn/belonged to");
+        
+        list.add("malt that lay in the house that Jack built.");
+        list.add("This is the house that Jack built.");
+        list.add("This is the malt that lay in the house that Jack built.");
+        list.add("rat/ate");
+        list.add("cat/killed");
+        list.add("dog/worried");
+        list.add("cow with the crumpled horn/tossed");
+        list.add("maiden all forlorn/milked");
+        list.add("man all tattered and torn/kissed");
+        list.add("priest all shaven and shorn/married");
+        list.add("rooster that crowed in the morn/woke");
+        list.add("farmer sowing his corn/kept");
+        list.add("horse and the hound and the horn/belonged to");
 
     }
 
     public String verse(int verseNumber) {
 
 
-        if (verseNumber == 1) return map.get(1);
+        if (verseNumber == 1) return list.get(1);
         
-        if (verseNumber == 2) return map.get(2);
+        if (verseNumber == 2) return list.get(2);
 
         StringBuilder builder = new StringBuilder();
 
         builder.append("This is the ");
 
         for (int i = verseNumber; i > 2; i--) {
-            String currLine = map.get(i);
+            String currLine = list.get(i);
             String[] tokens = currLine.split("/");
             
             builder.append(String.format("%s that %s the ", tokens[0], tokens[1]));
         }
 
-        builder.append(map.get(0));
+        builder.append(list.get(0));
         
         return builder.toString();
     }
@@ -61,7 +62,7 @@ public class House {
     public String sing() {
         
         StringBuilder builder = new StringBuilder();
-        int finalVerseIndex = map.size()-1;
+        int finalVerseIndex = list.size()-1;
         
         for (int i = 1; i <= finalVerseIndex; i++) {
             builder.append(verse(i));
